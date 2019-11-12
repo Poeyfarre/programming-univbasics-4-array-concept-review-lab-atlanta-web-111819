fbcdb84fdb83acc5572713fa75757c137010fb2c
Jacy Leech <jacy.atlanta@gmail.com>
2019-11-12 12:29:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,10 +4,10 @@ def find_element_index(array, value_to_find)
   end
 end
 
-def find_max_value(array)
-  puts array.max
-end
+# def find_max_value(array)
+#   puts array.max
+# end
 
-def find_min_value(array)
-  # Add your solution here
-end
+# def find_min_value(array)
+#   # Add your solution here
+# end
